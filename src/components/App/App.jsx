e76d056c0db092643301c54aa76ab0a994e30c92
Serhii Balaba <balabaserhii@gmail.com>
2024-05-25 16:54:48 +0200

    s

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import fetchFilms from "../../api-film"
+
+function App() {
+console.log(fetchFilms)
+  return (
+    <>
+  
+    </>
+  )
+}
+
+export default App
